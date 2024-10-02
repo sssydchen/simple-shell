f960d961b2603d783bfbadd37c340f2466d6d307
Sharan Parikh <parikh.shar@northeastern.edu>
2024-10-02 18:37:40 -0400

    Steps 1 and 2 initial changes

--- shell56.c
+++ shell56.c
@@ -5,7 +5,7 @@
  * Peter Desnoyers, Northeastern CS5600 Fall 2024
  */
 
-/* <> means don't check the local directory */ 
+/* <> means don't check the local directory */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -21,43 +21,130 @@
 #include <sys/wait.h>
 #include <signal.h>
 #include <fcntl.h>
+#include <linux/limits.h>
 
+int builtin_cd(int argc, char **argv)
+{
+    char *path = NULL;
+
+    if (argc == 1)
+    {
+        path = getenv("HOME");
+    }
+    else if (argc == 2)
+    {
+        path = argv[1];
+    }
+    else
+    {
+        fprintf(stderr, "cd: wrong number of arguments\n");
+        return 1;
+    }
+
+    int status = chdir(path);
+    if (status == 0)
+    {
+        printf("Present directory changed to: %s\n", path);
+    }
+    else
+    {
+        fprintf(stderr, "cd: %s\n", strerror(errno));
+    }
+    return status;
+}
+
+int builtin_pwd(int argc, char **argv)
+{
+    char buffer[PATH_MAX];
+    char *path = getcwd(buffer, sizeof(buffer));
+    if (path == NULL)
+    {
+        fprintf(stderr, "Could not determine present working directory\n");
+        return 1;
+    }
+    printf("Present working directory: %s\n", buffer);
+    return 0;
+}
+
+int builtin_exit(int argc, char **argv)
+{
+    if (argc == 1)
+    {
+        exit(0);
+    }
+    else if (argc == 2)
+    {
+        exit(atoi(argv[1]));
+    }
+    else
+    {
+        fprintf(stderr, "exit: too many arguments\n");
+        return 1;
+    }
+}
+
+int execute_command(int argc, char **argv)
+{
+    int status = -1;
+    char *built_in_commands[] = {"cd", "pwd", "exit"};
+
+    if (strcmp(argv[0], built_in_commands[0]) == 0)
+    {
+        status = builtin_cd(argc, argv);
+    }
+    else if (strcmp(argv[0], built_in_commands[1]) == 0)
+    {
+        status = builtin_pwd(argc, argv);
+    }
+    else if (strcmp(argv[0], built_in_commands[2]) == 0)
+    {
+        status = builtin_exit(argc, argv);
+    }
+
+    return status;
+}
 
 int main(int argc, char **argv)
 {
     bool interactive = isatty(STDIN_FILENO); /* see: man 3 isatty */
     FILE *fp = stdin;
 
-    if (argc == 2) {
+    if (argc == 2)
+    {
         interactive = false;
         fp = fopen(argv[1], "r");
-        if (fp == NULL) {
+        if (fp == NULL)
+        {
             fprintf(stderr, "%s: %s\n", argv[1], strerror(errno));
             exit(EXIT_FAILURE); /* see: man 3 exit */
         }
     }
-    if (argc > 2) {
+    if (argc > 2)
+    {
         fprintf(stderr, "%s: too many arguments\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    
+
     char line[1024], linebuf[1024];
     const int max_tokens = 32;
     char *tokens[max_tokens];
-    
+
     /* loop:
      *   if interactive: print prompt
      *   read line, break if end of file
      *   tokenize it
      *   print it out <-- your logic goes here
      */
-    while (true) {
-        if (interactive) {
+    while (true)
+    {
+        if (interactive)
+        {
             /* print prompt. flush stdout, since normally the tty driver doesn't
              * do this until it sees '\n'
              */
             printf("$ ");
             fflush(stdout);
+            signal(SIGINT, SIG_IGN);
         }
 
         /* see: man 3 fgets (fgets returns NULL on end of file)
@@ -71,12 +158,16 @@ int main(int argc, char **argv)
 
         /* replace the code below with your shell:
          */
-        printf("line:");
-        for (int i = 0; i < n_tokens; i++)
-            printf(" '%s'", tokens[i]);
-        printf("\n");
+        // printf("line:");
+        // for (int i = 0; i < n_tokens; i++)
+        //     printf(" '%s'", tokens[i]);
+        // printf("\n");
+        if (n_tokens > 0)
+        {
+            execute_command(n_tokens, tokens);
+        }
     }
 
-    if (interactive)            /* make things pretty */
-        printf("\n");           /* try deleting this and then quit with ^D */
+    if (interactive)  /* make things pretty */
+        printf("\n"); /* try deleting this and then quit with ^D */
 }
